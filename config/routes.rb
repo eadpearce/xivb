61faed35e92c7a45679c4619a078b441118a3df1
eadpearce <e.pearce@tutanota.com>
2017-07-12 12:50:46 +0100

    Working on auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'authentications/register'
-
-  get 'authentications/login'
-
-  resources :users
+  scope :api do
+    resources :users
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
