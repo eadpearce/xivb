7c611eba8168f048a535ecedaf40151e191a4084
eadpearce <e.pearce@tutanota.com>
2017-07-12 13:29:50 +0100

    Auth working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    render json: { errors: ["Unauthorized"] }, status: 401 unless user_signed_in?
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(username: decoded_token[:username]) if id_found?
+  rescue
+    nil
+  end
+
+  private
+
+  def id_found?
+    # puts "DECODED TOKEN: #{decoded_token}"
+    token && decoded_token && decoded_token[:username]
+  end
+
+  def decoded_token
+    @decoded_token ||= Auth.decode(token) if token
+  end
+
+  def token
+    @token ||= if request.headers['Authorization'].present?
+      request.headers['Authorization'].split.last
+    end
+  end
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,12 +1,12 @@
 class AuthenticationsController < ApplicationController
-  # skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
   def register
     user = User.new(user_params)
     if user.save
-      # token = Auth.issue({username: user.username})
+      token = Auth.issue({username: user.username})
       # render json: { token: token, user: UserSerializer.new(user) }, status: :ok
-      render json: { user: user }, status: :ok
+      render json: { token: token, user: user }, status: :ok
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -15,9 +15,9 @@ class AuthenticationsController < ApplicationController
   def login
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-    #   token = Auth.issue({username: user.username})
+      token = Auth.issue({username: user.username})
       # render json: { token: token, user: UserSerializer.new(user) }, status: :ok
-      render json: { user: user }, status: :ok
+      render json: { token: token, user: user }, status: :ok
     else
       render json: { errors: ["Invalid login credentials."]}, status: 401
     end
