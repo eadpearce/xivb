d47ac8fdb084d6e95fa8a37eedbb3f547e6caab5
eadpearce <e.pearce@tutanota.com>
2017-07-13 13:06:09 +0100

    Setting up

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    fetch('/api/users')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ users: data });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <section>
+          Hello world
+        </section>
+      </main>
     );
   }
+
 }
 
 export default App;
