2ba959f46009c899eb7aaf42bc6c53e1d13e60cf
eadpearce <e.pearce@tutanota.com>
2017-07-21 16:35:58 +0100

    More styling

--- client/src/css/classLists.js
+++ client/src/css/classLists.js
@@ -1,4 +1,4 @@
 const classLists = {
-  container: "w-100 w-75-ns center ph2 ph3-ns"
+  container: "w-100 w-75-ns center ph2 ph5-ns"
 };
 export default classLists;
--- client/src/pages/users/show.js
+++ client/src/pages/users/show.js
@@ -39,22 +39,38 @@ class User extends Component {
     return (this.state.loaded) ?
     (
       <main className={classLists.container}>
-        <h2 className="glow cinzel f1"><NavLink className="white hover-white" to={'/users/'+user.username}>{user.username}</NavLink>'s Profile</h2>
-        <h3 className="grd-gold play f4">Main Character: </h3>
+        <h1 className="glow cinzel f1"><NavLink className="white hover-white" to={'/users/'+user.username}>{user.username}</NavLink>'s Profile</h1>
+        {/* NAVIGATION HERE */}
+        <div className="fl db">
+          <Link className="grd-gold play f4 mb2" to={"/users/"+user.username+"/posts"}>
+          <img className="v-mid pr2" src="https://ffxiv.gamerescape.com/w/images/8/86/Main_Command_26_Icon.png"/>
+          Posts</Link>
+        </div>
+        <div className="fr db">
+          <Link className="grd-gold play f4 mb2" to="/users">
+          Back
+          <img className="v-mid pl2" src="https://ffxiv.gamerescape.com/w/images/5/5e/Main_Command_19_Icon.png"/>
+          </Link>
+        </div>
+
+        {/* USER INFO */}
+        <div className="cb w-100"><h2 className="grd-gold play f4 dib mt3">Age: </h2><p className="dib play grd-silver f4">&nbsp;{user.age}</p></div>
+        <h2 className="grd-gold play f4">About: </h2><p className="blog-post">{user.about}</p>
+
+        {/* CHARACTER INFO */}
+        <h2 className="grd-gold play f4 mt3">Main Character: </h2>
           <p><Link to={'/characters/'+user.main.id}>{user.main.data.name} <i>"{user.main.data.data.title}"</i></Link> on {user.main.data.server}</p>
-        <p><span className="grd-silver play f5">Main Job: </span>{user.main_job}</p>
-        <p><b>Alts: </b></p>
-        {user.alts.map(alt => {
+        <h2 className="grd-gold play f4 dib">Main Job: </h2><span className="grd-silver play f4"> {user.main_job}</span>
+        <h2 className="grd-gold play f4">Alts: </h2>
+        <p>{user.alts.map(alt => {
           return (alt.data.data.title) ?
             (<p key={alt.id}>
               <Link to={'/characters/'+alt.id}>{alt.data.name} <i>"{alt.data.data.title}"</i></Link> on {alt.data.server}</p>) :
             (<p key={alt.id}><Link to={'/characters/'+alt.id}>{alt.data.name}</Link> on {alt.data.server}</p>)
-        })}
-        <p><b>Age: </b>{user.age}</p>
-        <p><b>About: </b>{user.about}</p>
+        })}</p>
 
         {/* TOP 5 RECENT POSTS */}
-        <p className="b"><Link to={"/users/"+user.username+"/posts"}>Recent posts: </Link></p>
+        <h2 className="grd-gold play f4">Recent posts: </h2>
         {recentPosts.map(post => {
           if (post) {
             if (post.title) {
