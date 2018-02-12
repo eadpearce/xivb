37fbf53d0ff097a7c8bcf7371f1962edb150846a
eadpearce <e.pearce@tutanota.com>
2018-02-12 10:32:15 +0000

    Cleaned up character data caching function

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,12 +1,15 @@
 class Character < ApplicationRecord
   belongs_to :user, optional: true
   after_find do |character|
-  time_updated = Time.now # check current time
-  # only update character data if data hasn't already been updated in the last 24hrs or doesn't exist
-    if !character.data_updated or ((time_updated - character.data_updated)/(60*60*24) >= 1)
-      response = HTTParty.get("http://api.xivdb.com/character/#{character.lodestone_id}", format: :plain)
+    time_updated = Time.now # check current time
+    # only update character data if data hasn't already been updated in the
+    # last 24hrs or doesn't exist
+    days_since_update = (time_updated - character.data_updated) / (60 * 60 * 24)
+    if !character.data_updated || (days_since_update >= 1)
+      url = "http://api.xivdb.com/character/#{character.lodestone_id}"
+      response = HTTParty.get(url, format: :plain)
       data = JSON.parse(response)
-      puts "UPDATING CHARACTER DATA"
+      puts 'UPDATING CHARACTER DATA'
       character.data = data
       character.data_updated = time_updated
       character.save
